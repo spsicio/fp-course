780156ea63e08269977f15b21348b1d55b7b052d
spsicio <spsicio@foxmail.com>
2025-10-10 05:21:04 +0800

    chore: ignore .vscode and format haskell files

--- src/Course/FastAnagrams.hs
+++ src/Course/FastAnagrams.hs
@@ -1,11 +1,11 @@
-{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Course.FastAnagrams where
 
 import Course.Core
-import Course.List
 import Course.Functor
+import Course.List
 import qualified Data.Set as S
 
 -- Return all anagrams of the given string
@@ -13,24 +13,24 @@ import qualified Data.Set as S
 -- on a Mac - run this with:
 -- > fastAnagrams "Tony" "/usr/share/dict/words"
 fastAnagrams ::
-  Chars
-  -> FilePath
-  -> IO (List Chars)
+    Chars ->
+    FilePath ->
+    IO (List Chars)
 fastAnagrams =
-  error "todo: Course.FastAnagrams#fastAnagrams"
+    error "todo: Course.FastAnagrams#fastAnagrams"
 
-newtype NoCaseString =
-  NoCaseString
-    Chars
+newtype NoCaseString
+    = NoCaseString
+        Chars
 
 ncString ::
-  NoCaseString
-  -> Chars
+    NoCaseString ->
+    Chars
 ncString (NoCaseString s) =
-  s
+    s
 
 instance Eq NoCaseString where
-  (==) = (==) `on` map toLower . ncString
+    (==) = (==) `on` map toLower . ncString
 
 instance Show NoCaseString where
-  show = show . ncString
+    show = show . ncString
