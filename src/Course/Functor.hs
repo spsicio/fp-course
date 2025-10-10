3a098af877d57ce6daee63703dcc4695c1131e85
spsicio <spsicio@foxmail.com>
2025-10-11 00:11:50 +0800

    feat: Functor

--- src/Course/Functor.hs
+++ src/Course/Functor.hs
@@ -44,8 +44,7 @@ instance Functor ExactlyOne where
         (a -> b) ->
         ExactlyOne a ->
         ExactlyOne b
-    (<$>) =
-        error "todo: Course.Functor (<$>)#instance ExactlyOne"
+    (<$>) = mapExactlyOne
 
 {- | Maps a function on the List functor.
 
@@ -61,7 +60,7 @@ instance Functor List where
         List a ->
         List b
     (<$>) =
-        error "todo: Course.Functor (<$>)#instance List"
+        map
 
 {- | Maps a function on the Optional functor.
 
@@ -77,7 +76,7 @@ instance Functor Optional where
         Optional a ->
         Optional b
     (<$>) =
-        error "todo: Course.Functor (<$>)#instance Optional"
+        mapOptional
 
 {- | Maps a function on the reader ((->) t) functor.
 
@@ -89,8 +88,7 @@ instance Functor ((->) t) where
         (a -> b) ->
         ((->) t a) ->
         ((->) t b)
-    (<$>) =
-        error "todo: Course.Functor (<$>)#((->) t)"
+    (<$>) = (.)
 
 {- | Anonymous map. Maps a constant value on a functor.
 
@@ -106,8 +104,8 @@ prop> \x q -> x <$ Full q == Full x
     a ->
     k b ->
     k a
-(<$) =
-    error "todo: Course.Functor#(<$)"
+(<$) a =
+    (const a <$>)
 
 {- | Apply a value to a functor-of-functions.
 
@@ -132,8 +130,8 @@ Empty
     k (a -> b) ->
     a ->
     k b
-(??) =
-    error "todo: Course.Functor#(??)"
+(??) kf a =
+    ($ a) <$> kf
 
 infixl 1 ??
 
@@ -156,7 +154,7 @@ void ::
     k a ->
     k ()
 void =
-    error "todo: Course.Functor#void"
+    (() <$)
 
 -----------------------
 -- SUPPORT LIBRARIES --
