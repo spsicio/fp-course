02bd9f0e66f6c5c85d4a04781fc035fc37bc1ac0
spsicio <spsicio@foxmail.com>
2025-10-11 15:10:27 +0800

    feat: FileIO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -85,8 +85,9 @@ printFile ::
     FilePath ->
     Chars ->
     IO ()
-printFile =
-    error "todo: Course.FileIO#printFile"
+printFile fp chars = do
+    putStrLn $ "============ " ++ fp
+    putStrLn chars
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
@@ -94,15 +95,19 @@ printFiles ::
     List (FilePath, Chars) ->
     IO ()
 printFiles =
-    error "todo: Course.FileIO#printFiles"
+    void . sequence . (<$>) printFileAux
+  where
+    printFileAux (fp, chars) =
+        printFile fp chars
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
     FilePath ->
     IO (FilePath, Chars)
-getFile =
-    error "todo: Course.FileIO#getFile"
+getFile fp = do
+    chars <- readFile fp
+    pure (fp, chars)
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
@@ -110,21 +115,27 @@ getFiles ::
     List FilePath ->
     IO (List (FilePath, Chars))
 getFiles =
-    error "todo: Course.FileIO#getFiles"
+    sequence . (<$>) getFile
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@, @lines@, and @printFiles@.
 run ::
     FilePath ->
     IO ()
-run =
-    error "todo: Course.FileIO#run"
+run fp = do
+    chars <- readFile fp
+    let fps = lines chars
+    files <- getFiles fps
+    printFiles files
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
     IO ()
-main =
-    error "todo: Course.FileIO#main"
+main = do
+    args <- getArgs
+    case args of
+        fp :. xs -> run fp
+        Nil -> putStrLn "Usage: run <file>"
 
 ----
 
