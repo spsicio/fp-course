6709808c652cf370fbdf469071c5f3ab128110c4
spsicio <spsicio@foxmail.com>
2025-10-15 12:42:52 +0800

    feat: Compose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -20,8 +20,8 @@ instance
     (Functor f, Functor g) =>
     Functor (Compose f g)
     where
-    (<$>) =
-        error "todo: Course.Compose (<$>)#instance (Compose f g)"
+    (<$>) f (Compose fga) =
+        Compose $ (f <$>) <$> fga
 
 instance
     (Applicative f, Applicative g) =>
@@ -29,11 +29,11 @@ instance
     where
     -- Implement the pure function for an Applicative instance for Compose
     pure =
-        error "todo: Course.Compose pure#instance (Compose f g)"
+        Compose . pure . pure
 
     -- Implement the (<*>) function for an Applicative instance for Compose
-    (<*>) =
-        error "todo: Course.Compose (<*>)#instance (Compose f g)"
+    (<*>) (Compose fgh) (Compose fga) =
+        Compose $ (<*>) <$> fgh <*> fga
 
 instance
     (Monad f, Monad g) =>
@@ -41,7 +41,7 @@ instance
     where
     -- Implement the (=<<) function for a Monad instance for Compose
     (=<<) =
-        error "todo: Course.Compose (=<<)#instance (Compose f g)"
+        error "impossible"
 
 -- Note that the inner g is Contravariant but the outer f is
 -- Functor. We would not be able to write an instance if both were
@@ -51,5 +51,5 @@ instance
     Contravariant (Compose f g)
     where
     -- Implement the (>$<) function for a Contravariant instance for Compose
-    (>$<) =
-        error "todo: Course.Compose (>$<)#instance (Compose f g)"
+    (>$<) h (Compose fga) =
+        Compose $ (h >$<) <$> fga
